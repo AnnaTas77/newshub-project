6dd5e4e50a3f1c7d67490fd373f40ca7035b0b25
Anna Tasheva <anna.tasheva90@gmail.com>
2024-09-10 11:18:25 +0100

    Added simple styling for Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,13 @@
+import styled from "@emotion/styled";
 import React from "react";
 
+const FooterContainer = styled.footer`
+  padding: 30px 0 30px 0;
+  border-top: 1px solid #ced4da;
+  margin-top: 50px;
+  text-align: center;
+`;
+
 export const Footer: React.FC = () => {
-  return <div>Footer</div>;
+  return <FooterContainer>Footer</FooterContainer>;
 };

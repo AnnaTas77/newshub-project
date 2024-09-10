dcb0b35a2f253f441f62321b19da8bce9d9b9361
Anna Tasheva <anna.tasheva90@gmail.com>
2024-09-10 10:33:55 +0100

    Added basic styles to the Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,56 @@
 import Link from "next/link";
 import React from "react";
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+
+const NavContainer = styled.nav`
+  display: flex;
+  padding: 1rem 0;
+  justify-content: flex-end;
+  align-items: center;
+  border-bottom: 1px solid #ced4da;
+  position: fixed;
+  width: 100%;
+  top: 0;
+  background: white;
+  z-index: 100;
+`;
+
+const NavbarList = styled.ul`
+  display: flex;
+  align-items: center;
+  list-style: none;
+  padding: 0;
+  margin: 0 20px;
+  gap: 30px;
+`;
+
+const Button = styled.button`
+  font-size: 1.1rem;
+  background-color: #0b7285;
+  font-weight: 900;
+  padding: 12px;
+  color: whitesmoke;
+  border-radius: 8px;
+  border: none;
+`;
 
 const Navbar: React.FC = () => {
   return (
-    <nav>
-      <ul>
+    <NavContainer>
+      <NavbarList>
         <li>
           <Link href="/">
-            <button>Home</button>
+            <Button>Home</Button>
           </Link>
         </li>
         <li>
           <Link href="/admin">
-            <button>Admin</button>
+            <Button>Admin</Button>
           </Link>
         </li>
-      </ul>
-    </nav>
+      </NavbarList>
+    </NavContainer>
   );
 };
 

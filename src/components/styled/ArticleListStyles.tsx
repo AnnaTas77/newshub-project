7379fcc4b0f5015b1a773a81289a677af1b9ca61
Anna Tasheva <anna.tasheva90@gmail.com>
2024-09-17 22:33:24 +0100

    Removed a bug with empty grid columns appearing on wider screens

--- src/components/styled/ArticleListStyles.tsx
+++ src/components/styled/ArticleListStyles.tsx
@@ -7,12 +7,12 @@ export const ArticleContainerStyle = styled.section<{ isAdmin: boolean }>`
   grid-template-columns: ${({ isAdmin }) =>
     isAdmin
       ? "none"
-      : "repeat(auto-fill, minmax(250px, 275px))"}; /* Responsive columns */
+      : "repeat(auto-fit, minmax(250px, 275px))"}; /* Responsive columns */
   padding: 25px 10px;
-  gap: 5px;
+  gap: 15px;
   justify-content: center;
   width: 75%;
-  max-width: ${({ isAdmin }) => (isAdmin ? "1000px" : "none")};
+  max-width: ${({ isAdmin }) => (isAdmin ? "1000px" : "1500px")};
   align-items: center;
   margin: 30px 0;
 `;

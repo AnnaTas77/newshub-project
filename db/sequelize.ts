28061516d2c5b02cd5c227946cbb57e1a63c3d53
Anna Tasheva <anna.tasheva90@gmail.com>
2024-09-25 21:11:50 +0100

    Configure database path to use environment variable and dynamic fallback

--- db/sequelize.ts
+++ db/sequelize.ts
@@ -1,13 +1,20 @@
 // Database Connection
 import { Sequelize } from "sequelize";
 import path from "path";
+import dotenv from "dotenv";
 
-const DB_PATH_CONFIG = "/Users/anna.tasheva/Desktop/newshub-project/db/";
+// Load environment variables from .env file
+dotenv.config();
 
+const DYNAMIC_DB_PATH_CONFIG = path.join(process.cwd(), "db");
+const DB_PATH_CONFIG = process.env.HARD_CODED_DB_PATH || DYNAMIC_DB_PATH_CONFIG;
+
+// Use the environment variable if it exists, otherwise use the hard-coded path
 const dbPath = path.join(DB_PATH_CONFIG, "database.sqlite");
+
 const sequelize = new Sequelize({
   dialect: "sqlite",
-  storage: dbPath, // Path to the SQLite database file
+  storage: dbPath,
 });
 
 // to verify that the connection to the database can be established successfully
